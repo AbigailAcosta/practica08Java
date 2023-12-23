7ada083c00183bd0e591ea273a19d04ba8f01f36
AbigailAcosta <acostadianaabi13@gmail.com>
2023-12-23 15:29:25 -0300

    Practica 08 en Java - Mini encuesta

---
+++ src/practica08/Practica08.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica08;
+
+import practica08.Swing.frmEncuesta;
+
+/**
+ *
+ * @author Abigail Acosta
+ */
+public class Practica08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmEncuesta formulario = new frmEncuesta();
+        formulario.setVisible(true);
+    }
+    
+}
